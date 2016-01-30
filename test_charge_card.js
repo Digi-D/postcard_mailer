5086addd25ace4c5b2fd329e44d0c7f49115c4cb
Dmitry Strakovsky <shifting.planes@gmail.com>
2016-01-30 18:13:48 -0500

    Set up Stripe test microsite

--- test_charge_card.js
+++ test_charge_card.js
@@ -1,1 +1,34 @@
-//testing Stripe Functionality 
+//testing Stripe Functionality
+
+var fs = require('fs');
+eval(fs.readFileSync('./my_keys.js').toString()); //throw your keys here
+var express = require('express');
+var hbars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var stripe = require("stripe")(TEST_STRIPE_SECRET_KEY);
+
+var app = express();
+
+var json_parser = bodyParser.json();
+app.engine('handlebars',hbars({defaultLayout: false}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function (req, res) {
+   res.render( __dirname + "/" + "simple_payment_form", {stripe_key:TEST_STRIPE_PUBLISHABLE_KEY});
+})
+
+app.post('/finalize_payment', json_parser, function (req, res) {
+  console.log(req.body.tokenid);
+  res.end('polo');
+  //console.log(req);
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+
+})
