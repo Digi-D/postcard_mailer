a01dc0dbb95eb948bfe06cf3b100fe77bfa81b44
dima <shifting.planes@gmail.com>
2016-02-11 10:52:31 -0500

    added flightplan to the project

---
+++ app_modules/config.js
@@ -0,0 +1,13 @@
+//all configuration settings and keys
+module.exports =
+{
+  'TEST_LOB_KEY':'test_721a627a37b16853eb3dc464c93834fedfb',
+  'PROD_LOB_KEY':'0',
+
+  'TEST_STRIPE_SECRET_KEY':'sk_test_3XJAXEALLTn7hRwONkA91PyB',
+  'TEST_STRIPE_PUBLISHABLE_KEY':'pk_test_AnN5EyTAXijWCz9NbgpDpGX4',
+  'PROD_STRIPE_SECRET_KEY':'0',
+  'PROD_STRIPE_PUBLISHABLE_KEY':'0',
+  
+  //MANDRILL KEYS
+}
