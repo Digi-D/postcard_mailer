c07f24d33ecb7b91e8fef54706e0cdb107c84592
dima <shifting.planes@gmail.com>
2016-02-15 14:19:13 -0500

    general housekeeping- new folders and modules

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 //NEED TO: add form validation, decent design, installation shots from sites
-
-
 var express = require('express');
 var app = express();
+var hbars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var config = require('./config.js'); //throw your keys and other settings here
 
-//app.use(express.static('public'));
+app.engine('handlebars',hbars({defaultLayout: false}));
+app.set('view engine', 'handlebars');
+
 
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
+// present the project and address/number of postcards to the viewers
+   res.sendFile( __dirname + "/views/" + "index.html" );
+})
+app.get('/confirm', function (req, res) {
+//confirm address, number of postcard and price with the viewiers
+   res.sendFile( __dirname + "/" + "confirm.html" );
 })
 
-app.get('/process_get', function (req, res) {
+app.get('/result', function (req, res) {
+//report result of the card charge and postcard send requests to the viewers
+   res.sendFile( __dirname + "/" + "result.html" );
+})
 
-   // Prepare output in JSON format
+app.get('/get_visitor_info', function (req, res) {
+
    response = {
       visitor_name:req.query.visitor_name,
       visitor_address:req.query.visitor_address,
---
+++ snippets/README.md
@@ -0,0 +1,3 @@
+Random code from previous test and iterations related to this project.
+Most not functional at this point but shows general strategies.
+ 
