f6bdc1febd13451d7ad9112887727de9b221c94b
dima <shifting.planes@gmail.com>
2016-02-18 11:54:28 -0500

    basic server validation done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //NEED TO: add decent design, installation shots from sites
+//Questions: How to validate address?
 var express = require('express');
 var app = express();
 var hbars = require('express-handlebars');
@@ -28,37 +29,24 @@ app.get('/result', function (req, res) {
 
 app.get('/get_visitor_info', function (req, res) {
 //recieve, validate and render out for confimation by user all of the parameters of the order
-  var final_price = req.query.number_of_cards*config.PRICE_PER_CARD;
+
   var visitor_info = {
     first_name:req.query.visitor_first_name,
     last_name:req.query.visitor_last_name,
+    email:req.query.visitor_email,
     address_1:req.query.visitor_address_1,
     address_2:req.query.visitor_address_2,
     city:req.query.visitor_city,
     state:req.query.visitor_state,
     zip:req.query.visitor_zip,
     number_of_cards:req.query.number_of_cards,
-    price:final_price,
+    price:0 //just a placeholder for now make this prettier!
   }
-  var confirmationMessage = function (params_to_validate, path_to_confirmation_view, callback) {
-    this.params_to_validate = params_to_validate;
-    var are_params_valid = true;
-    console.log(this.params_to_validate.first_name);
-    callback(path_to_confirmation_view, {
-      visitor_info:this.params_to_validate,
-      params_valid:are_params_valid
-    });
-  }
-//
-  var generateConfirmView = function(path, validation_state){
+
+  validate.confirmationMessage(visitor_info, __dirname + "/views/" + "confirm", function(path, validation_state){
     res.render(path, validation_state);
-  }
+  });
 
-  confirmationMessage(visitor_info, __dirname + "/views/" + "confirm", generateConfirmView);
-
-
-
-  //console.log(confirmation_messages.params_valid);
 })
 
 
--- app_modules/validate_form.js
+++ app_modules/validate_form.js
@@ -1,10 +1,68 @@
-var validateParams= {
-  process:function(input_params) {
-    var are_params_valid = true;
-    console.log(input_params.first_name);
-    return are_params_valid;
-  }
+var validator = require('validator');
+var config = require('./config.js');
 
+var isValidUSZip = function(sZip) {
+   return /^\d{5}(-\d{4})?$/.test(sZip);
 }
 
-module.exports= Object.create(validateParams)
+module.exports= {
+
+  confirmationMessage: function (params_to_validate, path_to_confirmation_view, callback) {
+    this.params_to_validate = params_to_validate;
+    var country = "US";
+    var are_params_valid = true;
+    if(!validator.isAlpha(this.params_to_validate.first_name)){
+      are_params_valid= false;
+    }
+    else if(!validator.isAlpha(this.params_to_validate.last_name)){
+      are_params_valid= false;
+    }
+    else if(!validator.isEmail(this.params_to_validate.email)){
+      are_params_valid= false;
+    }
+    else if(!validator.isAlpha(this.params_to_validate.city)){
+      are_params_valid= false;
+    }
+    else if(!validator.isAlpha(this.params_to_validate.state)){
+      are_params_valid= false;
+    }
+    else if(!validator.isInt(this.params_to_validate.zip)){
+      are_params_valid= false;
+    }
+    else if(!validator.isInt(this.params_to_validate.number_of_cards)){
+      are_params_valid= false;
+    }
+    else{
+      //basic checks done | check zip and number_of_cards and bake correct price
+      if(country=="US"){
+        if(isValidUSZip(this.params_to_validate.zip)){
+          are_params_valid= true;
+        }
+        else{
+          are_params_valid = false;
+        }
+      }
+
+      if(this.params_to_validate.number_of_cards>0){
+          this.params_to_validate.price = this.params_to_validate.number_of_cards*config.PRICE_PER_CARD;
+          are_params_valid = true;
+      }
+      else {
+        are_params_valid= false;
+      }
+
+
+
+
+
+    }
+
+
+    callback(path_to_confirmation_view, {
+      //pass all of the form inputs and validation state to
+      //handlebars engine to render
+      visitor_info:this.params_to_validate,
+      params_valid:are_params_valid
+    });
+  },
+}
