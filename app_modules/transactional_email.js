2c657dd4aad18020d38ee5f79a6d17e40b9b4ffd
dima <shifting.planes@gmail.com>
2016-02-21 17:51:04 -0500

    minor updates to transactional email library

--- app_modules/transactional_email.js
+++ app_modules/transactional_email.js
@@ -9,19 +9,19 @@ module.exports = {
   mail_processed_succes_admin_update:{
     from: 'INFINITE.INDUSTRIES <info@infinite.industries>',
     to: ['shifting.planes@gmail.com'],
-    subject: 'Problem with your order',
-    text: 'Unfortunately, one or more cards in your order has not been processed properly. We will review the details of the order and contact you as soon as possible.'
+    subject: 'Successfull Order',
+    text: 'Quick order summary'
   },
   mail_processed_failure:{
     from: 'INFINITE.INDUSTRIES <info@infinite.industries>',
     to: ['shifting.planes@gmail.com'],
-    subject: 'Hello',
-    text: 'Successfull order of cards by visitor. Check logs! '
+    subject: 'Order Update',
+    text: 'Unfortunately, one or more cards in your order has not been processed properly. We will review the details of the order and contact you as soon as possible.'
   },
   mail_processed_failure_admin_update:{
     from: 'INFINITE.INDUSTRIES <info@infinite.industries>',
     to: ['shifting.planes@gmail.com'],
-    subject: 'Hello',
-    text: 'Testing some Mailgun awesomness!'
+    subject: 'ALERT!!!! something went wrong with an order',
+    text: 'Quick order summary and contact info for the visitor'
   },
 }
