48f5c81a45d94384ed0735d52628100be0f023ba
Rafalskyi Oleksandr <orafalskyi@gmail.com>
2016-02-25 21:43:48 -0500

    master async example implemented

--- app_modules/mail_postcards.js
+++ app_modules/mail_postcards.js
@@ -3,47 +3,45 @@ var config = require('./config.js'); //throw your keys here
 var Lob = require('lob')(config.TEST_LOB_KEY);
 var async = require('async');
 
-var sendPostcard = function(mail_description,mail_to){
+var sendPostcard = function (mail_description, mail_to, cb) {
   Lob.postcards.create({
-    description:mail_description,
-    to:mail_to,
-    front: fs.readFileSync('./media/LOB_template.png'),
-    back: '<html style="padding-left:0.3in;padding-top:0.5in"><div style="font-size:0.3in">The International</div><div style="font-size:15px;padding-top:0.1in; padding-left:0.2in;font-family:sans-serif;">infinite.industries/the-international</div></html>',
+      description: mail_description,
+      to: mail_to,
+      front: fs.readFileSync('./media/LOB_template.png'),
+      back: '<html style="padding-left:0.3in;padding-top:0.5in"><div style="font-size:0.3in">The International</div><div style="font-size:15px;padding-top:0.1in; padding-left:0.2in;font-family:sans-serif;">infinite.industries/the-international</div></html>',
+    },
+    function (err, res) {
+      console.log(err);
+      cb(err, res);   // !  - if you need success action  cb(null, res);
+    });
+};
+
+var repeat = 3;
+
+var mail_params = {};
+var status = 'success';
+
+module.exports = {
+  status: status,
+  init: function (params) {
+    mail_params = params;
   },
-  function (err, res) {
-    console.log(err, res);
-  })
-}
-
-
-var mailPostcards = {
-  mail_params:{},
-  init:function(params,cue_length){
-    this.mail_params = {
-      description: 'Demo Postcard job6',
-      //skipping "from" attribute
-      to: {
-        name: 'Joe Smith',
-        address_line1: '123 Main Street',
-        address_city: 'Mountain View',
-        address_state: 'CA',
-        address_zip: '94041'
-      }
-    };
-    
-  },
-  process_cue:function(){
-
-  },
-  send:function(){
-    async.retry(
-      {times: 3, interval: 100},
-      sendPostcard(this.mail_params.description,this.mail_params.to),
-      function(err, result) {
-      // do something with the result
-      });
-
+  send: function (callback) {
+    var result;
+    var counter = 1;
+    for (var i = 0; i < mail_params.queue_length; i++) {
+      async.retry(
+        {times: repeat, interval: 100},
+        function (callback, results) {
+          sendPostcard(mail_params.description, mail_params.to, callback);
+        },
+        function (err, result) {
+          if (err) status = 'failure';
+          if (counter == mail_params.queue_length) {
+            callback(err, status);
+          }
+          counter++;
+        });
+    }
   }
-}
-
-module.exports = Object.create(mailPostcards);
+};
--- test_postcard.js
+++ test_postcard.js
@@ -1,5 +1,22 @@
-var mailPostcards = require('./app_modules/mail_postcards.js');
+var mailPostcards = require('./app_modules/mail_postcards');
 
+var mail_params = {
+  description: 'Demo Postcard job6',
+  //skipping "from" attribute
+  to: {
+    name: 'Joe Smith',
+    address_line1: '123 Main Street',
+    address_city: 'Mountain View',
+    address_state: 'CA',
+    address_zip: '94041'
+  },
+  queue_length: 5
+};
 
-mailPostcards.init();
-mailPostcards.send();
+mailPostcards.init(mail_params);
+callback = function(err, result) {
+  console.log('we GOT IT!!!! -->', result);
+};
+
+mailPostcards.send(callback);
+
